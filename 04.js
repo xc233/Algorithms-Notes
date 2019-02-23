adc642dcb25e95e2e16febd25c1635ed34ded81a
xc233 <quantsleeper@gmail.com>
2019-02-23 20:34:48 +0800

    ac1

--- 04.js
+++ 04.js
@@ -1,4 +1,4 @@
-let target = 5;
+let target = 32;
 let matrix = [
     [1,   4,  7, 11, 15],
     [2,   5,  8, 12, 19],
@@ -10,21 +10,21 @@ function Find(target , matrix) {
     if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
         return false;
     let cols = matrix.length - 1;
-    for(i = 0, j = cols; i >=0 && j <=cols ; ){
+    for(i = 0, j = cols; j >=0 && i <=cols ; ){
         if (target == matrix[i][j]){
-            console.log("Find");
+            //console.log("Find");
             return true;
         }else if(target < matrix[i][j]){
-            console.log("Delete Right colums");
+            //console.log("Delete Right colums");
             j--;
             continue;
         }else if(target > matrix[i][j]){
-            console.log("Delete left rows");
+            //console.log("Delete left rows");
             i++;
             continue;
         }
     }
-    console.log("no find");
+    //console.log("no find");
     return false;
 }
 Find (target,matrix);
